28722bf74c3615324f5dee7edfc70f935f878d36
wawrzyniak julien <wawrzyniakjulien@gmail.com>
2018-01-24 21:45:22 -0500

    Add webpack v1.0

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,27 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin")
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: './src/Index.tsx',
-    output: {
-        filename: './dist/js/bundle.js'
+	devtool: "source-map",
+	output: {
+        path: __dirname + '/build',
+        filename: 'js/bundle.min.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss']
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss', '.gz']
     },
     module: {
         rules: [
             {test: /\.tsx?$/, loader: 'ts-loader'},
-            {test: /\.(css|scss)$/,
-                use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader', options: {
-                        sourceMap: true
-                    }
-                }, {
-                    loader: 'sass-loader', options: {
-                        sourceMap: true
-                    }
-                }]
+            {test: /\.scss$/,
+	            use: ExtractTextPlugin.extract({
+		            fallback: 'style-loader',
+		            use: [{loader: 'css-loader', options: { minimize: true }}, 'sass-loader']
+	            })
             }
         ]
-    }
+    },
+	plugins: [
+	    new HtmlWebpackPlugin({
+		    template: 'index.template.ejs'
+	    }),
+	    new ExtractTextPlugin('css/style.min.css'),
+	    new UglifyJsPlugin(),
+	    new CompressionPlugin({
+		    asset: "[path].gz[query]",
+		    algorithm: 'gzip'
+	    }),
+	    new CopyWebpackPlugin([
+		    { from: 'assets/', to: 'assets/'}
+		])
+		]
 };
