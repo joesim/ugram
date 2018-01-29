17680b52fb3fe9f871af8026e0678779eb4b07c8
Bruno LL <brunoll96@outlook.com>
2018-01-28 22:54:17 -0500

    encryption

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,44 +1,55 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CompressionPlugin = require("compression-webpack-plugin")
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const  CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = {
-    entry: './src/Index.tsx',
-	devtool: "source-map",
-	output: {
-        path: __dirname + '/build',
-        filename: 'js/bundle.min.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss', '.gz']
-    },
-    module: {
-        rules: [
-            {test: /\.tsx?$/, loader: 'ts-loader'},
-            {test: /\.scss$/,
-	            use: ExtractTextPlugin.extract({
-		            fallback: 'style-loader',
-		            use: [{loader: 'css-loader', options: { minimize: true }}, 'sass-loader']
-	            })
-            }
-        ]
-    },
-	plugins: [
-	    new HtmlWebpackPlugin({
-			template: 'index.template.ejs'
-	    }),
-	    new ExtractTextPlugin('css/style.min.css'),
-		new UglifyJsPlugin(),
-		/*
-	    new CompressionPlugin({
-		    asset: "[path][query]",
-			algorithm: 'gzip',
-		}),
-		*/
-	    new CopyWebpackPlugin([
-		    { from: 'assets/', to: 'assets/'}
-		])
-	]
-};
+module.exports = function(env) {
+	if (typeof env == 'undefined') {
+		env = {};
+	}
+
+	config = {
+		entry: './src/Index.tsx',
+		devtool: "source-map",
+		output: {
+			path: __dirname + '/build',
+			filename: 'js/bundle.min.js'
+		},
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss', '.gz']
+		},
+		module: {
+			rules: [
+				{test: /\.tsx?$/, loader: 'ts-loader'},
+				{test: /\.scss$/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: [{loader: 'css-loader', options: { minimize: true }}, 'sass-loader']
+					})
+				}
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: 'index.template.ejs'
+			}),
+			new ExtractTextPlugin('css/style.min.css'),
+			new UglifyJsPlugin(),
+			new CopyWebpackPlugin([
+				{ from: 'assets/', to: 'assets/'}
+			])
+		]
+	};
+
+	if (env.travis) {
+		config.plugins.push(
+			new CompressionPlugin({
+				asset: "[path][query]",
+				algorithm: 'gzip',
+			})
+		)
+	}
+
+	return config;
+}
