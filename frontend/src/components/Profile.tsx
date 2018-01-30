c8c9eb5290d5f21779b12d98ddd014d41cdd6ba3
Joel Simard <14114114+Othack@users.noreply.github.com>
2018-01-30 01:21:19 -0500

    profile page responsive

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import Avatar from "material-ui/Avatar";
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 
-const avatarStyle = {
-    width: "200px",
-    height: "200px"
+const avatarStyle: Object = {
+    width: "125px",
+    height: "125px"
 }
 
 const rowUser: Object = {
@@ -15,12 +16,27 @@ const rowUser: Object = {
     flexFlow: "row wrap",
 }
 
+const rowNoSpace: Object = {
+    justifyContent: "center",
+    display: "flex",
+    alignItems: "center",
+    flexFlow: "row wrap",
+}
+
+const rowNoWrap: Object = {
+    justifyContent: "space-around",
+    display: "flex",
+    alignItems: "center",
+    flexFlow: "row nowrap",
+}
+
 const avatarItem: Object = {
     padding: "20px"
 }
 
 const infoItem: Object = {
-    padding: "20px"
+    padding: "10px",
+    alignSelf: "flex-start"
 }
 
 const rowUsername: Object = {
@@ -28,23 +44,56 @@ const rowUsername: Object = {
     flexFlow: "row wrap",
 }
 
-const Profile = () => {
-    return (
-        <div className="container mt-25">
-            <div style={rowUser}>
-                <div style={avatarItem}>
-                    <Avatar className="text-xs-center" style={avatarStyle} src="https://memegenerator.net/img/images/600x600/1137321/xzibit-yo-dawg.jpg" />
-                </div>
-                <div style={infoItem}>
-                    <div style={rowUser}>
-                        <h4>Xzibit Xzibot</h4>
-                        <p className="ma-20">@xzibit</p>
-                        <RaisedButton labelPosition="before" label="Edit" primary={true} icon={<FontIcon className="material-icons">edit</FontIcon>}></RaisedButton>
+const cardStyle: Object = {
+    width: "100%",
+    margin: "20px",
+    height: "500px"
+}
+
+class Profile extends React.Component {
+    public render(){
+        return (
+            <div className="container mt-25">
+            <div className="flex-container">
+                <div style={rowUser}>
+                    <div style={avatarItem}>
+                        <Avatar className="text-xs-center" style={avatarStyle} src="https://memegenerator.net/img/images/600x600/1137321/xzibit-yo-dawg.jpg" />
+                    </div>
+                    <div style={infoItem}>
+                        <div style={rowNoSpace} className="mb-10">
+                            <div className="h4 font-weight-bold">Lorem Ipsum</div>
+                            <p className="ml-10 mr-30">@xzibit</p>
+                            <RaisedButton labelPosition="before" label="Edit" primary={true} icon={<FontIcon className="material-icons">edit</FontIcon>}></RaisedButton>
+                        </div>
+                        <div style={rowUser}>
+                            <div style={rowNoWrap} className="ml-30 mr-30">
+                                <FontIcon className="material-icons mr-10 ml-10">email</FontIcon>
+                                <div>
+                                    xzibit@hotmail.com
+                                </div>
+                            </div>
+                            <div style={rowNoWrap}>
+                                <FontIcon className="material-icons mr-10 ml-10">phone</FontIcon>
+                                <div>
+                                    +1 (418) 123-4567
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     </div>
                 </div>
+                <div style={rowUser}>
+                        <Card style={cardStyle}>
+                        <CardText>
+                          Images here
+                        </CardText>
+                      </Card>
+                        </div>
             </div>
-        </div>
-    )
-};
+        );
+        
+    }
+}
+
 
 export { Profile };
