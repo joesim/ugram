888e478dd5a0da29c30704a4aca17d089d84ac1b
Gabriel Legault <gablegault1@hotmail.com>
2018-02-05 20:44:28 -0500

    avancement de l'upload
    [ci skip]

--- frontend/stories/index.stories.js
+++ frontend/stories/index.stories.js
@@ -4,6 +4,9 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
+import UploadModal from './UploadModal';
+import UploadButton from './UploadButton';
+
 
 storiesOf('Welcome', module)
   .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
@@ -12,3 +15,6 @@ storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
   
+storiesOf('Modal', module)
+  .add('Modal for image upload', () => <UploadModal />)
+  .add('Upload button', () => <UploadButton />)
