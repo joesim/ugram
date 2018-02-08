42b6dc2db79d5b4ca21c23b41dc3835c373cf264
Bruno <brunoll96@outlook.com>
2018-02-07 22:12:46 -0500

    Ajout du lien à la liste des users

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Avatar from 'material-ui/Avatar';
 import List, { ListItem } from 'material-ui/List';
+import { Link } from "react-router-dom";
 
 interface Props {
 	getAllUsers: any,
@@ -20,11 +21,13 @@ class Users extends React.Component<Props, any> {
         return (
             <List>
                 {this.props.users.map((user) => 
-                    <ListItem 
-                    key={user.id} 
-                    primaryText={user.firstName + " " + user.lastName}
-                    leftAvatar={<Avatar src={user.pictureUrl} />} 
-                    />
+                    <Link className="list-link-style-override" to={`/users/${user.id}`}>
+                        <ListItem 
+                        key={user.id} 
+                        primaryText={`${user.firstName} ${user.lastName}`}
+                        leftAvatar={<Avatar src={user.pictureUrl} />} 
+                        />
+                    </Link>
                 )}
             </List>
         );
