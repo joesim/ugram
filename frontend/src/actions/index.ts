6ed3822a6b77ebe1c5b8fc91c351962de778133b
Gabriel Legault <gablegault1@hotmail.com>
2018-02-09 12:46:42 -0500

    it should work

--- frontend/src/actions/index.ts
+++ frontend/src/actions/index.ts
@@ -1,8 +1,10 @@
 import {AppBarAction, setVisibilityMenu, setVisibilityUploadModal} from "./AppBar";
-import {editProfile, profileFetchData, ProfilePanelActions} from "./Profile";
 import {HomeAction} from "./Home";
-import {PicturesPanelAction, getAllPictures, getAllPicturesFromUser} from "./PicturesPanel";
+import {deletePicture, editPicture, PictureDetailsAction} from "./PictureDetails";
+import {getAllPictures, getAllPicturesFromUser, PicturesPanelAction} from "./PicturesPanel";
+import {editProfile, profileFetchData, ProfilePanelActions} from "./Profile";
 import {SignupAction, signupUser} from "./Signup";
-import {PictureDetailsAction, editPicture, deletePicture} from "./PictureDetails";
 
-export { AppBarAction, setVisibilityMenu, setVisibilityUploadModal, editProfile, profileFetchData, ProfilePanelActions, HomeAction, PicturesPanelAction, getAllPictures, SignupAction,signupUser, PictureDetailsAction, editPicture, deletePicture, getAllPicturesFromUser};
+export { AppBarAction, setVisibilityMenu, setVisibilityUploadModal, editProfile, profileFetchData,
+         ProfilePanelActions, HomeAction, PicturesPanelAction, getAllPictures, SignupAction, signupUser,
+         PictureDetailsAction, editPicture, deletePicture, getAllPicturesFromUser};
