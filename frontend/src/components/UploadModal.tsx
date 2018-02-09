65ee9ba720ba1a9e3aed14a3fc2805cb5bbde99a
Gabriel Legault <gablegault1@hotmail.com>
2018-02-09 14:22:23 -0500

    trying to access action from a child component
    [ci skip]

--- frontend/src/components/UploadModal.tsx
+++ frontend/src/components/UploadModal.tsx
@@ -1,18 +1,50 @@
 import Avatar from "material-ui/Avatar";
 import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import RaisedButton from "material-ui/RaisedButton";
 import FileUpload from "material-ui/svg-icons/file/file-upload";
 import * as React from "react";
 
-const UploadModal = ( {visibility}: any ) => {
-    return (
-        <Dialog
-              title="Upload an image"
-              modal={true}
-              open={visibility}
-        >
-            <Avatar icon={<FileUpload />} />
-        </Dialog>
-    );
-};
+interface IComponentNameProps {
+    visibility: boolean;
+}
 
-export default UploadModal;
+export default class UploadModal extends React.Component<IComponentNameProps, any> {
+    constructor(props: IComponentNameProps) {
+        super(props);
+    }
+    public handleClose = () => {
+        console.log("Il faudrait caller onFileUploadModalClick ici");
+      }
+
+    public render(): JSX.Element {
+        const actions = [
+            (
+                <FlatButton
+                  label="Cancel"
+                  primary={true}
+                  onClick={this.handleClose}
+                />
+            ),
+            (
+                <FlatButton
+                  label="Submit"
+                  primary={true}
+                  disabled={true}
+                  onClick={this.handleClose}
+                />
+            ),
+        ];
+
+        return (
+            <Dialog
+              title="Upload an image"
+              actions={actions}
+              modal={true}
+              open={this.props.visibility}
+            >
+              <Avatar icon={<FileUpload />} />
+            </Dialog>
+        );
+    }
+}
