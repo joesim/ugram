e7bf0e8e10ea438454d82751f0ab9359a9720204
Joel Simard <14114114+Othack@users.noreply.github.com>
2018-02-10 22:44:35 -0500

    navbar

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,25 +1,27 @@
 import * as React from "react";
 
 import { AppBar, Drawer, MenuItem } from "material-ui";
+import FontIcon from 'material-ui/FontIcon';
 
 import { Link } from "react-router-dom";
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+const stle = {
+    width: "950px"
+}
 
-const AppBarUgram = ( { appbar, onLeftIconButtonClick }: any) => {
+const AppBarUgram = ({ appbar, onLeftIconButtonClick }: any) => {
     return (
         <div>
-            <AppBar
-                title="Ugram"
-                onLeftIconButtonClick={onLeftIconButtonClick}
-            />
-            <Drawer open={appbar.menu.isVisible}>
-                <AppBar
-                    title="Ugram"
-                    onLeftIconButtonClick={onLeftIconButtonClick}
-                />
-                <Link to="/pictures"><MenuItem>Pictures</MenuItem></Link>
-                <Link to="/profile"><MenuItem>Profile</MenuItem></Link>
-                <Link to="/users"><MenuItem>Users</MenuItem></Link>
-            </Drawer>
+            <div className="navbar">
+                <div className="container flex-justify-between flex-align-items-center height-100">
+                    <Link to="/" className="no-decoration"><div className="items-navbar title-navbar">UGram</div></Link>
+                    <div>
+                    <Link to="/"><FontIcon className="material-icons items-navbar">collections</FontIcon></Link>
+                    <Link to="/"><FontIcon className="material-icons items-navbar">group</FontIcon></Link>
+                    <Link to="/"><FontIcon className="material-icons items-navbar">person</FontIcon></Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
