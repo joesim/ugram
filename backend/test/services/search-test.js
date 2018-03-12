112f97066b3ce3db7870b9d2acb967a685967fdf
Gabriel Legault <gablegault1@hotmail.com>
2018-03-12 15:44:18 -0400

    testing the ci with first test(it should not pass)

--- backend/test/services/search-test.js
+++
@@ -1,6 +0,0 @@
-import sinon from 'sinon';
-import chai from 'chai';
-import mongoose from 'mongoose';
-import * as sinonMongoose from 'sinon-mongoose';
-
-const expect = chai.expect;
---
+++ backend/test/services/searchTest.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const chai = require("chai");
+const mongoose = require('mongoose');
+const sinonMongoose= require('sinon-mongoose');
+
+const expect = chai.expect;
+
+describe("Route /search", () => {
+  describe("given", () => {
+    it("The test should fail", () => {
+      expect(2).to.equal(3);
+    });
+  });
+});
