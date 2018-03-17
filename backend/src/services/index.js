3c47d804a6f3e4dfcc45a16105651198ebe20c40
wawrzyniak julien <wawrzyniakjulien@gmail.com>
2018-03-17 14:21:38 -0400

    remove useless comments

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -5,13 +5,7 @@ const parseEntry = (data) => {
 		let jsonData = data.toJSON();
 		delete jsonData._id;
 		delete jsonData.__v;
-		// if (typeof jsonData.registrationDate !== undefined) {
-		// 	jsonData.registrationDate = jsonData.registrationDate.getTime();
-		// }
-		// 	if (typeof jsonData.createdDate !== undefined) {
-		// 		jsonData.createdDate = jsonData.createDate.getTime();
-		// 	}
-		//
+
 		return jsonData;
 };
 
