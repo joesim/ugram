3c57553eb9f657944074e2ed3ae4c75fc0c5bca0
Gabriel Legault <gablegault1@hotmail.com>
2018-04-09 16:46:22 -0400

    stable version of ConnectedClientsModel

--- backend/src/controllers/clients.js
+++ backend/src/controllers/clients.js
@@ -1,43 +1,69 @@
 import { ConnectedClientsModel, UserModel } from "../models";
+import logger from "../common/logger";
+import { mongoose } from "../common/mongoose";
 
-const connectClient = async(socketId, data) => {
-    try {
-        const accessToken = data.accessToken;
-        const userModel = await UserModel.findOne({accessToken});
-        const userId = userModel.id;
+const connectClient = async (socketId, data) => {
+  try {
+    const accessToken = data.accessToken;
+    const userModel = await UserModel.findOne({ accessToken });
+    const userId = userModel.id;
 
-        const connectedClientsModel = await findOneOrCreate(ConnectedClientsModel, userId);
-        const connectedInstances = [...connectedClientsModel.connectedInstances, socketId];
-        // TODO the model doesn't update
-        await connectedClientsModel.update({userId:userId}, {connectedInstances: connectedInstances});
-    } catch (error) {
-        
+    const connectedClientsModel = await findOneOrCreate(
+      ConnectedClientsModel,
+      userId
+    );
+    const connectedInstances = [
+      ...connectedClientsModel.connectedInstances,
+      socketId
+    ];
+    await ConnectedClientsModel.findOneAndUpdate(
+      { userId: userId },
+      { connectedInstances: connectedInstances }
+    );
+    logger.info(`${userId} est connecté avec ${connectedInstances.length} instance(s)`);
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+const disconnectClient = async socketId => {
+  try {
+    const connectedClientsModel = await ConnectedClientsModel.findOne({
+      connectedInstances: socketId
+    });
+
+    const connectedInstances = connectedClientsModel.connectedInstances;
+    removeClient(connectedInstances, socketId);
+    const userId = connectedClientsModel.userId;
+
+    await ConnectedClientsModel.findOneAndUpdate(
+      { userId: userId },
+      { connectedInstances: connectedInstances }
+    );
+    logger.info(`${userId} est connecté avec ${connectedInstances.length} instance(s)`)
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+const removeClient = (connectedInstances, socketId) => {
+    const idx = connectedInstances.indexOf(socketId);
+    if (idx != -1) {
+        connectedInstances.splice(idx, 1);
     }
-
 }
 
-const disconnectClient = async(socketId) => {
-    try {
-        console.log('ici');
-        const connectedClientsModel = await ConnectedClientsModel.findOne({socketId});
-        const index = connectedClientsModel.connectedInstances.indexOf(socketId);
-        const connectedInstances = connectedClientsModel.connectedInstances;
-        console.log(connectedClientsModel);
-        if(index != -1) {
-            connectedInstances.splice(index, 1);
-        }
-        await connectedClientsModel.update({userId:userId}, {connectedInstances: connectedInstances});
-    } catch (error) {
-        
+const findOneOrCreate = async (model, id) => {
+  let modelFound;
+  try {
+    modelFound = await model.findOne({ userId: id });
+    if (modelFound === null) {
+      modelFound = await model.create({ userId: id });
     }
-    
-}
-
-const findOneOrCreate = async(model, id) => {
-    const modelFound = await model.findOne({id}) === null ? await model.create({userId:id}) : await model.findOne({id});
-    console.log(modelFound);
-    return modelFound;
-}
-
+  } catch (error) {
+    logger.error(error);
+  }
+  return modelFound;
+};
 
 export { connectClient, disconnectClient };
